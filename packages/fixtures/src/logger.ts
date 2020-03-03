2eb7db2d4abb8c14498ee72e8351ef7a1be6e060
Béré Cyriac <cyriacbr@gmail.com>
2020-03-03 15:13:02 +0000

    feat(fixtures): added logger

---
+++ packages/fixtures/src/logger.ts
@@ -0,0 +1,11 @@
+import * as chalk from "chalk";
+
+export class Logger {
+    error(msg: string, ...context: any[]) {
+        console.log(`${chalk.red("[FixturesFactory]")} - ${msg}`);
+        if (context && context.length > 0) {
+            console.log('Provided context: ', context);
+        }
+        throw new Error(msg);
+    }
+}
