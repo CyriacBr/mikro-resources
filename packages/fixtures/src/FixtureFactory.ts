3ce46602d286494746ed0ac2839837ea6c844dbf
Tolga Paksoy <tolga@paramander.com>
2021-04-03 21:12:41 +0200

    feat: factory method chaining to allow .with and .ignore into .persist methods (#12)
    * chore: fix lint warning on function var
    
    * feat: allow chaining factory result methods
    
    fixes #11
    
    * test: add tests for chaining factory methods into persist

--- packages/fixtures/src/FixtureFactory.ts
+++ packages/fixtures/src/FixtureFactory.ts
@@ -7,20 +7,7 @@ import {
 import { MetadataStore } from './MetadataStore';
 import { MikroORM, Collection } from '@mikro-orm/core';
 import { EntityClass } from '@mikro-orm/core/typings';
-
-type DeepPartial<T> = T extends unknown
-  ? unknown
-  : {
-      [P in keyof T]?: DeepPartial<T[P]>;
-    };
-export interface FactoryResult<T> {
-  one: () => T;
-  many: (x: number) => T[];
-  oneAndPersist: () => Promise<T>;
-  manyAndPersist: (x: number) => Promise<T[]>;
-  with: (input: DeepPartial<T>) => FactoryResult<T>;
-  ignore: (...props: (keyof T)[]) => FactoryResult<T>;
-}
+import { FactoryResultWrapper } from './FactoryResultWrapper';
 
 export class FixtureFactory {
   private factory: FF;
@@ -40,7 +27,7 @@ export class FixtureFactory {
   private registerAllEntities() {
     const metadata = this.orm.getMetadata();
     const entityNames = Object.keys(metadata.getAll()).filter(
-      v => v[0] === v[0].toUpperCase()
+      (v) => v[0] === v[0].toUpperCase()
     );
     for (const name of entityNames) {
       const classType = metadata.get(name).class;
@@ -59,24 +46,7 @@ export class FixtureFactory {
 
   make<Entity = object>(
     entityName: EntityClass<Entity>
-  ): FactoryResult<Entity> {
-    const baseResult = this.factory.make(entityName as any);
-
-    return <FactoryResult<Entity>>{
-      ignore: baseResult.ignore as any,
-      with: baseResult.with as any,
-      many: baseResult.many as any,
-      one: baseResult.one as any,
-      oneAndPersist: async () => {
-        const entity = baseResult.one();
-        await this.orm.em.persistAndFlush(entity);
-        return entity;
-      },
-      manyAndPersist: async (x: number) => {
-        const entities = baseResult.many(x);
-        await this.orm.em.persistAndFlush(entities);
-        return entities;
-      },
-    };
+  ): FactoryResultWrapper<Entity> {
+    return new FactoryResultWrapper(this.orm, this.factory, entityName);
   }
 }
